ac22b89387f3e37a66b526ff6a51d5fcef07ffdf
hijihyun <67703614+hijihyun@users.noreply.github.com>
2021-06-24 20:49:05 +0900

    git page 배포

--- docs/fin/app.js
+++ docs/fin/app.js
@@ -1,1 +1,283 @@
-!function(t){var e={};function n(r){if(e[r])return e[r].exports;var l=e[r]={i:r,l:!1,exports:{}};return t[r].call(l.exports,l,l.exports,n),l.l=!0,l.exports}n.m=t,n.c=e,n.d=function(t,e,r){n.o(t,e)||Object.defineProperty(t,e,{enumerable:!0,get:r})},n.r=function(t){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},n.t=function(t,e){if(1&e&&(t=n(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var l in t)n.d(r,l,function(e){return t[e]}.bind(null,l));return r},n.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return n.d(e,"a",e),e},n.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},n.p="",n(n.s=0)}([function(t,e,n){"use strict";n.r(e);var r=function(t,e){return new Date(t.date).getTime()<new Date(e.date).getTime()?1:-1};let l=null,o=null,a="",c="";const d=document.querySelector("#studyFilters").children,i=d.length,s=document.querySelector("#quizFilters").children,u=s.length,f=document.querySelector("#studyLoading"),g=document.querySelector("#studyTable"),h=document.querySelector("#quizLoading"),y=document.querySelector("#quizTable"),$=document.querySelector("#tStudyBody"),b=document.querySelector("#tQuizBody");function p(t){f.style.display="",g.style.display="none",setTimeout(v,500),d[0].className=d[0].className.replace(" active",""),l&&(l.className=l.className.replace(" active",""));const e=t.currentTarget;var n;e.className+=" active",l=e,$.innerHTML="",n=e.id,fetch("class.json").then((function(t){t.json().then((function(t){console.log("json data:",t),"studyAllBtn"===n?j(t):"studyLinkBtn"===n?function(t){for(let e=0;e<t.length;e++)if(0!==t[e].links.length){a+=`\n    <tr>\n      <th scope="row">${e+1}</th>\n      <td>${t[e].title}</td>\n      <td>\n      <a href="${t[e].docUrl}"\n      class="badge bg-secondary">\n      문서</a></td>\n      <td>`;for(let n=0;n<t[e].links.length;n++)a+=`\n        <a href="${t[e].links[n]}"\n        class="badge bg-secondary">${n+1}\n        </a>`;a+=`</td><td>${t[e].date}</td><td>`,t[e].gitUrl.length>0&&(a+=`<a href="${t[e].gitUrl}">git</a>`),a+="</td></tr>"}$.innerHTML=a,a=""}(t):"studyGitBtn"===n?function(t){for(let e=0;e<t.length;e++)if(0!==t[e].gitUrl.length){a+=`\n    <tr>\n      <th scope="row">${e+1}</th>\n      <td>${t[e].title}</td>\n      <td>\n      <a href="${t[e].docUrl}"\n      class="badge bg-secondary">\n      문서</a></td>\n      <td>`;for(let n=0;n<t[e].links.length;n++)a+=`\n        <a href="${t[e].links[n]}"\n        class="badge bg-secondary">${n+1}\n        </a>`;a+=`</td><td>${t[e].date}</td><td>`,t[e].gitUrl.length>0&&(a+=`<a href="${t[e].gitUrl}">git</a>`),a+="</td></tr>"}$.innerHTML=a,a=""}(t):"studyNewBtn"===n&&function(t){t.sort(r);for(let e=0;e<t.length;e++){a+=`\n    <tr>\n      <th scope="row">${e+1}</th>\n      <td>${t[e].title}</td>\n      <td>\n      <a href="${t[e].docUrl}"\n      class="badge bg-secondary">\n      문서</a></td>\n      <td>`;for(let n=0;n<t[e].links.length;n++)a+=`\n        <a href="${t[e].links[n]}"\n        class="badge bg-secondary">${n+1}\n        </a>`;a+=`</td><td>${t[e].date}</td><td>`,t[e].gitUrl.length>0&&(a+=`<a href="${t[e].gitUrl}">git</a>`),a+="</td></tr>"}$.innerHTML=a,a=""}(t)})).catch((function(t){console.log("Fetch Error :-S",t)}))}))}function m(t){h.style.display="",y.style.display="none",setTimeout(U,500),s[0].className=s[0].className.replace(" active",""),o&&(o.className=o.className.replace(" active",""));const e=t.currentTarget;var n;e.className+=" active",o=e,b.innerHTML="",n=e.id,fetch("quiz.json").then((function(t){t.json().then((function(t){console.log("json data:",t),"quizAllBtn"===n?S(t):"quizGitBtn"===n&&function(t){for(let e=0;e<t.length;e++)0!==t[e].gitUrl.length&&(c+=`\n    <tr>\n      <td>${t[e].title}</td>\n      <td>\n      <a href="${t[e].docUrl}"\n      class="badge bg-secondary">\n      문서</a></td>\n      <td><a href="${t[e].previewUrl}">보기</a></td>\n      <td><a href="${t[e].gitUrl}">git</a></td>\n    </tr>`);b.innerHTML=c,c=""}(t)})).catch((function(t){console.log("Fetch Error :-S",t)}))}))}for(let t=0;t<i;t++)d[t].addEventListener("click",p);for(let t=0;t<u;t++)s[t].addEventListener("click",m);function v(){f.style.display="none",g.style.display=""}function U(){h.style.display="none",y.style.display=""}function j(t){for(let e=0;e<t.length;e++){if(a+=`\n    <tr>\n      <th scope="row">${e+1}</th>\n      <td>${t[e].title}</td>\n      <td>\n      <a href="${t[e].docUrl}"\n      class="badge bg-secondary">\n      문서</a></td>\n      <td>`,t[e].links.length>0)for(let n=0;n<t[e].links.length;n++)a+=`\n        <a href="${t[e].links[n]}"\n        class="badge bg-secondary">${n+1}\n        </a>`;a+=`</td><td>${t[e].date}</td><td>`,t[e].gitUrl.length>0&&(a+=`<a href="${t[e].gitUrl}">git</a>`),a+="</td></tr>"}$.innerHTML=a,a=""}function S(t){for(let e=0;e<t.length;e++)c+=`\n    <tr>\n      <td>${t[e].title}</td>\n      <td>\n      <a href="${t[e].docUrl}"\n      class="badge bg-secondary">\n      문서</a></td>\n      <td><a href="${t[e].previewUrl}">보기</a></td>\n      <td><a href="${t[e].gitUrl}">git</a></td>\n    </tr>`;b.innerHTML=c,c=""}v(),U(),fetch("class.json").then((function(t){t.json().then((function(t){console.log("json data:",t),j(t)})).catch((function(t){console.log("Fetch Error :-S",t)}))})),fetch("quiz.json").then((function(t){t.json().then((function(t){console.log("json data:",t),S(t)})).catch((function(t){console.log("Fetch Error :-S",t)}))}))}]);
+!(function (t) {
+	var e = {};
+	function n(r) {
+		if (e[r]) return e[r].exports;
+		var l = (e[r] = { i: r, l: !1, exports: {} });
+		return t[r].call(l.exports, l, l.exports, n), (l.l = !0), l.exports;
+	}
+	(n.m = t),
+		(n.c = e),
+		(n.d = function (t, e, r) {
+			n.o(t, e) ||
+				Object.defineProperty(t, e, { enumerable: !0, get: r });
+		}),
+		(n.r = function (t) {
+			'undefined' != typeof Symbol &&
+				Symbol.toStringTag &&
+				Object.defineProperty(t, Symbol.toStringTag, {
+					value: 'Module',
+				}),
+				Object.defineProperty(t, '__esModule', { value: !0 });
+		}),
+		(n.t = function (t, e) {
+			if ((1 & e && (t = n(t)), 8 & e)) return t;
+			if (4 & e && 'object' == typeof t && t && t.__esModule) return t;
+			var r = Object.create(null);
+			if (
+				(n.r(r),
+				Object.defineProperty(r, 'default', {
+					enumerable: !0,
+					value: t,
+				}),
+				2 & e && 'string' != typeof t)
+			)
+				for (var l in t)
+					n.d(
+						r,
+						l,
+						function (e) {
+							return t[e];
+						}.bind(null, l),
+					);
+			return r;
+		}),
+		(n.n = function (t) {
+			var e =
+				t && t.__esModule
+					? function () {
+							return t.default;
+					  }
+					: function () {
+							return t;
+					  };
+			return n.d(e, 'a', e), e;
+		}),
+		(n.o = function (t, e) {
+			return Object.prototype.hasOwnProperty.call(t, e);
+		}),
+		(n.p = ''),
+		n((n.s = 0));
+})([
+	function (t, e, n) {
+		'use strict';
+		n.r(e);
+		var r = function (t, e) {
+			return new Date(t.date).getTime() < new Date(e.date).getTime()
+				? 1
+				: -1;
+		};
+		let l = null,
+			o = null,
+			a = '',
+			c = '';
+		const d = document.querySelector('#studyFilters').children,
+			i = d.length,
+			s = document.querySelector('#quizFilters').children,
+			u = s.length,
+			f = document.querySelector('#studyLoading'),
+			g = document.querySelector('#studyTable'),
+			h = document.querySelector('#quizLoading'),
+			y = document.querySelector('#quizTable'),
+			$ = document.querySelector('#tStudyBody'),
+			b = document.querySelector('#tQuizBody');
+		function p(t) {
+			(f.style.display = ''),
+				(g.style.display = 'none'),
+				setTimeout(v, 500),
+				(d[0].className = d[0].className.replace(' active', '')),
+				l && (l.className = l.className.replace(' active', ''));
+			const e = t.currentTarget;
+			var n;
+			(e.className += ' active'),
+				(l = e),
+				($.innerHTML = ''),
+				(n = e.id),
+				fetch('class.json').then(function (t) {
+					t.json()
+						.then(function (t) {
+							console.log('json data:', t),
+								'studyAllBtn' === n
+									? j(t)
+									: 'studyLinkBtn' === n
+									? (function (t) {
+											for (let e = 0; e < t.length; e++)
+												if (0 !== t[e].links.length) {
+													a += `\n    <tr>\n      <th scope="row">${
+														e + 1
+													}</th>\n      <td>${
+														t[e].title
+													}</td>\n      <td>\n      <a href="${
+														t[e].docUrl
+													}"\n      class="badge bg-secondary">\n      문서</a></td>\n      <td>`;
+													for (
+														let n = 0;
+														n < t[e].links.length;
+														n++
+													)
+														a += `\n        <a href="${
+															t[e].links[n]
+														}"\n        class="badge bg-secondary">${
+															n + 1
+														}\n        </a>`;
+													(a += `</td><td>${t[e].date}</td><td>`),
+														t[e].gitUrl.length >
+															0 &&
+															(a += `<a href="${t[e].gitUrl}">git</a>`),
+														(a += '</td></tr>');
+												}
+											($.innerHTML = a), (a = '');
+									  })(t)
+									: 'studyGitBtn' === n
+									? (function (t) {
+											for (let e = 0; e < t.length; e++)
+												if (0 !== t[e].gitUrl.length) {
+													a += `\n    <tr>\n      <th scope="row">${
+														e + 1
+													}</th>\n      <td>${
+														t[e].title
+													}</td>\n      <td>\n      <a href="${
+														t[e].docUrl
+													}"\n      class="badge bg-secondary">\n      문서</a></td>\n      <td>`;
+													for (
+														let n = 0;
+														n < t[e].links.length;
+														n++
+													)
+														a += `\n        <a href="${
+															t[e].links[n]
+														}"\n        class="badge bg-secondary">${
+															n + 1
+														}\n        </a>`;
+													(a += `</td><td>${t[e].date}</td><td>`),
+														t[e].gitUrl.length >
+															0 &&
+															(a += `<a href="${t[e].gitUrl}">git</a>`),
+														(a += '</td></tr>');
+												}
+											($.innerHTML = a), (a = '');
+									  })(t)
+									: 'studyNewBtn' === n &&
+									  (function (t) {
+											t.sort(r);
+											for (let e = 0; e < t.length; e++) {
+												a += `\n    <tr>\n      <th scope="row">${
+													e + 1
+												}</th>\n      <td>${
+													t[e].title
+												}</td>\n      <td>\n      <a href="${
+													t[e].docUrl
+												}"\n      class="badge bg-secondary">\n      문서</a></td>\n      <td>`;
+												for (
+													let n = 0;
+													n < t[e].links.length;
+													n++
+												)
+													a += `\n        <a href="${
+														t[e].links[n]
+													}"\n        class="badge bg-secondary">${
+														n + 1
+													}\n        </a>`;
+												(a += `</td><td>${t[e].date}</td><td>`),
+													t[e].gitUrl.length > 0 &&
+														(a += `<a href="${t[e].gitUrl}">git</a>`),
+													(a += '</td></tr>');
+											}
+											($.innerHTML = a), (a = '');
+									  })(t);
+						})
+						.catch(function (t) {
+							console.log('Fetch Error :-S', t);
+						});
+				});
+		}
+		function m(t) {
+			(h.style.display = ''),
+				(y.style.display = 'none'),
+				setTimeout(U, 500),
+				(s[0].className = s[0].className.replace(' active', '')),
+				o && (o.className = o.className.replace(' active', ''));
+			const e = t.currentTarget;
+			var n;
+			(e.className += ' active'),
+				(o = e),
+				(b.innerHTML = ''),
+				(n = e.id),
+				fetch('quiz.json').then(function (t) {
+					t.json()
+						.then(function (t) {
+							console.log('json data:', t),
+								'quizAllBtn' === n
+									? S(t)
+									: 'quizGitBtn' === n &&
+									  (function (t) {
+											for (let e = 0; e < t.length; e++)
+												0 !== t[e].gitUrl.length &&
+													(c += `\n    <tr>\n      <td>${t[e].title}</td>\n      <td>\n      <a href="${t[e].docUrl}"\n      class="badge bg-secondary">\n      문서</a></td>\n      <td><a href="${t[e].previewUrl}">보기</a></td>\n      <td><a href="${t[e].gitUrl}">git</a></td>\n    </tr>`);
+											(b.innerHTML = c), (c = '');
+									  })(t);
+						})
+						.catch(function (t) {
+							console.log('Fetch Error :-S', t);
+						});
+				});
+		}
+		for (let t = 0; t < i; t++) d[t].addEventListener('click', p);
+		for (let t = 0; t < u; t++) s[t].addEventListener('click', m);
+		function v() {
+			(f.style.display = 'none'), (g.style.display = '');
+		}
+		function U() {
+			(h.style.display = 'none'), (y.style.display = '');
+		}
+		function j(t) {
+			for (let e = 0; e < t.length; e++) {
+				if (
+					((a += `\n    <tr>\n      <th scope="row">${
+						e + 1
+					}</th>\n      <td>${
+						t[e].title
+					}</td>\n      <td>\n      <a href="${
+						t[e].docUrl
+					}"\n      class="badge bg-secondary">\n      문서</a></td>\n      <td>`),
+					t[e].links.length > 0)
+				)
+					for (let n = 0; n < t[e].links.length; n++)
+						a += `\n        <a href="${
+							t[e].links[n]
+						}"\n        class="badge bg-secondary">${
+							n + 1
+						}\n        </a>`;
+				(a += `</td><td>${t[e].date}</td><td>`),
+					t[e].gitUrl.length > 0 &&
+						(a += `<a href="${t[e].gitUrl}">git</a>`),
+					(a += '</td></tr>');
+			}
+			($.innerHTML = a), (a = '');
+		}
+		function S(t) {
+			for (let e = 0; e < t.length; e++)
+				c += `\n    <tr>\n      <td>${t[e].title}</td>\n      <td>\n      <a href="${t[e].docUrl}"\n      class="badge bg-secondary">\n      문서</a></td>\n      <td><a href="${t[e].previewUrl}">보기</a></td>\n      <td><a href="${t[e].gitUrl}">git</a></td>\n    </tr>`;
+			(b.innerHTML = c), (c = '');
+		}
+		v(),
+			U(),
+			fetch('class.json').then(function (t) {
+				t.json()
+					.then(function (t) {
+						console.log('json data:', t), j(t);
+					})
+					.catch(function (t) {
+						console.log('Fetch Error :-S', t);
+					});
+			}),
+			fetch('quiz.json').then(function (t) {
+				t.json()
+					.then(function (t) {
+						console.log('json data:', t), S(t);
+					})
+					.catch(function (t) {
+						console.log('Fetch Error :-S', t);
+					});
+			});
+	},
+]);
